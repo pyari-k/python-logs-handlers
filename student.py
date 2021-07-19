8cb5906aae5979fcdacbe11ef9d49e10f24ad249
Pyari Kumaran <pyari.kumaran@primarybid.com>
2021-07-20 03:06:01 +0530

    also added stream handlers apart from file handlers

--- student.py
+++ student.py
@@ -1,4 +1,5 @@
 import logging
+import sys
 logging.basicConfig(level=logging.INFO, format="%(name)s: %(message)s", filename="student.log")
 logger = logging.getLogger(__name__)
 
@@ -8,7 +9,13 @@ formatter = logging.Formatter(FORMAT)
 file_handler = logging.FileHandler('student_handler.log')
 file_handler.setFormatter(formatter)
 
+stream_handler = logging.StreamHandler(sys.stdout) #we now have a stream handler also
+stream_handler.setFormatter(formatter)
+
+
 logger.addHandler(file_handler)
+logger.addHandler(stream_handler) # also adding the stream handler
+
 
 class student:
     def __init__(self, first, last):
